d15a0465b5704a24d93bf8fa185aecf02bc58e4b
AliEmara <70377134+hithmast@users.noreply.github.com>
2021-02-10 17:00:04 +0200

    Update SwedeyPerc.py

--- SwedeyPerc.py
+++ SwedeyPerc.py
@@ -8,10 +8,10 @@ def percent_(product): #Simple Function to Calculate Our Math
     return()
 
 solid = { #Solid List for Swedey CopperWire Coated With PVC Max 75 C
-    "1mm": 250.80, "1.5mm": 361.38,"2mm":479.94, "3mm": 691.98, "4mm": 893.76, "6mm": 1362.30,"10mm":2200.20,"16mm":3470.16
+    "1mm": 251, "1.5mm": 362,"2mm":480, "3mm": 692, "4mm": 894, "6mm": 1363,"10mm":2201,"16mm":3471
     }
 flexible = { #Flixeble List for Swedey CopperWire Coated With PVC Max 75 C
-    "1mm": 285.91,"1.5mm":412, "2mm": 547.14, "3mm": 788.88, "4mm": 1018.93, "6mm": 1553.02, "10mm": 2508, "16mm": 3955.80
+    "1mm": 286,"1.5mm":412, "2mm": 548, "3mm": 789, "4mm": 1019, "6mm": 1554, "10mm": 2508, "16mm": 3956
     }
 
 for items in solid: 
